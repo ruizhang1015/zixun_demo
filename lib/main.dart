3ec9950682af42fb6eeefbbe44244eb90c3d38d2
Arlo Zheng <Arlo.Zheng@harman.com>
2024-11-12 11:42:20 +0800

    -

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {1
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
